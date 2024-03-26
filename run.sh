cdae8115de85d654e68f8f4b593f7f5b5bd424ef
Andrew Kelton <109199143+AndrewKelton@users.noreply.github.com>
2024-03-26 17:57:42 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 # !/bin/bash
 
-python3 main.py "city" FORECAST API_KEY
+echo City
+read city
+echo "FORECAST or Date (2024-09-28)" 
+read type
+echo "API KEY"
+read API_KEY
+
+python3 main.py "$city" $type $API_KEY
